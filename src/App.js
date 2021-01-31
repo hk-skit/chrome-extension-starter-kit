cef5692fb7aa94b0aee251d39280864e1e71fe81
Stefan Kudev <stefan.yanchev.kudev@gmail.com>
2021-01-31 00:22:16 +0000

    add brum js logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./brum_js.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -8,16 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Great! You've made it this far{" "}
+          <span role="img" aria-label="sunglasses guy">
+            😎
+          </span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
