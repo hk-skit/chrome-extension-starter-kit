6018864944feadd1391cbb7c812e6d36ca776783
Stefan Kudev <stefan.yanchev.kudev@gmail.com>
2021-01-31 00:40:01 +0000

    add button + styling

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import React from "react";
 import logo from "./brum_js.svg";
 import "./App.css";
 
+function printHelloWorld() {
+  console.log("Hello World!");
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <main className="container">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Great! You've made it this far{" "}
-          <span role="img" aria-label="sunglasses guy">
+          Great! You've made it this far
+          <span role="img" aria-label="sunglasses emoji">
             😎
           </span>
         </p>
-      </header>
+        <button onClick={printHelloWorld}>Print "Hello World!"</button>
+      </main>
     </div>
   );
 }
